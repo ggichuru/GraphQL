b115e43b65c41c50565861ac016a00dc0919a94b
ggichuru <ggichuru.dev@gmail.com>
2021-12-15 16:42:38 +0300

    mutations: creating comment

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -18,6 +18,8 @@ const typeDefs = `
 
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+        createComment(comment: String!, author: ID!, post: ID!): Comment!
     }
 
     type User {
@@ -191,6 +193,54 @@ const resolvers = {
             users.push(user)
 
             return user
+        },
+        createPost: (parent, args, ctx, info) => {
+            const userExists = users.some((user) => user.id === args.author)
+
+            if (!userExists) {
+                throw new Error('User not found')
+            }
+
+            const post = {
+                id: uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author
+            }
+
+            posts.push(post)
+
+            return post
+        },
+        createComment: (parent, args, ctx, info) => {
+            const userExists = users.some((user) => user.id === args.author)
+
+            if (!userExists) {
+                throw new Error('User not found')
+            }
+
+            const isPublishedPost = posts.some((post) => {
+                const isPost = post.id === args.post
+                const postPublished = post.published === true
+
+                return isPost && postPublished
+            })
+
+            if (!isPublishedPost) {
+                throw new Error('Either posts does not exist or is not published')
+            }
+
+            const comment = {
+                id: uuidv4(),
+                comment: args.comment,
+                author: args.author,
+                post: args.post
+            }
+
+            comments.push(comment)
+
+            return comment
         }
     },
     Post: {
