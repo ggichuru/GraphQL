5e5f103bae75635f81a67792a15e8a511e2fac40
ggichuru <ggichuru.dev@gmail.com>
2021-12-16 13:06:16 +0300

    Subscriptions: intro

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,23 +1,28 @@
 /**@ggichuru */
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import Mutation from "./resolvers/Mutation";
 import Query from "./resolvers/Query";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post"
 import Comment from "./resolvers/Comment";
+import Subscription from "./resolvers/Subscription";
+
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         User,
         Post,
         Comment
     },
     context: {
-        db
+        db,
+        pubsub
     }
 })
 
